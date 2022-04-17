8843ffe6b60a3eb2e485ba436a6be97b12cafe97
Luke Wardle <l.wardle@live.co.uk>
2022-04-18 00:11:58 +0100

    a command-line program that traverses all folders in the stores directory and lists all the sales.json files that were found.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFiles("stores");
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+// Program iterates through the folders storing the individual sales.json files from the store 
+// folder and save it the above variable salesFiles for printing.
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it.
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
